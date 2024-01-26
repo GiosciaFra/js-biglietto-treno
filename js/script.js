f6995e19344113f4a19030462ab4cf7a09476eb4
giosciafra <francescogioscia8@gmail.com>
2024-01-26 18:47:02 +0100

    aggiunte verifiche numeriche e alert

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ let eta = prompt("Inserisci l'età del passeggero:");
 
 // Calcola il prezzo del biglietto prima dello sconto
 let prezzoTotale = km * 0.21;
+
+//!isNaN verifica se entrambi i valori di 'eta' e 'km' sono numerici
  
+if (!isNaN(eta) && !isNaN(km) && (eta > 0 && eta < 130)) {
 
 
     // Se l'età è inferiore a 18, si applica uno sconto del 20%.
@@ -21,6 +24,9 @@ let prezzoTotale = km * 0.21;
         prezzoTotale = prezzoTotale - (prezzoTotale / 100) * 40
     }
 
-    document.getElementById("costoTicket").innerHTML = prezzoTotale.toFixed(2);
+    document.getElementById("costoFinale").innerHTML = prezzoTotale.toFixed(2);
    
+}  else {
 
+    alert("Inserimento non valido. Assicurati di inserire valori numerici validi per chilometri ed età.");
+}
